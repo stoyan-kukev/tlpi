1cc10ee840b6d415b420b52ed43b6e37773a54d2
stoyan-kukev <stoykukev@proton.me>
2024-04-07 19:53:04 +0300

    exercise 4.1 done

--- main.c
+++ main.c
@@ -1,88 +1,55 @@
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
 
+#include "lib/tlpi_common.h"
 #include "lib/error_functions.h"
-#include "lib/get_num.h"
 
 int main(int argc, char* argv[]) {
-    if (argc < 3 || strcmp(argv[1], "--help") == 0) {
-        usage_err("%s file {r<len>|R<len>|w<str>|s<offset>}...\n", argv[0]);
-    }
-
-    const char* file_name = argv[1];
-    int fd = open(
-        file_name,
-        O_RDWR | O_CREAT,
-        S_IRUSR | S_IWUSR |
-        S_IRGRP | S_IWGRP |
-        S_IROTH | S_IWOTH
-    );
-
-    if (fd == -1) {
-        err_exit("opening file");
+    if (argc == 1) {
+        usage_err("%s [-a/--append] file\n", argv[0]);
     }
     
-    size_t len;
-    char* buffer;
-    int bytes_written;
-    int offset;
-    for (int arg_pair = 2; arg_pair < argc; arg_pair += 1) {
-        switch(argv[arg_pair][0]) {
-            case 'r':
-            case 'R':
-                len = get_long(&argv[arg_pair][1], GN_ANY_BASE, argv[arg_pair]); 
-                buffer = malloc(len);
-                if (buffer == NULL) {
-                    err_exit("malloc");
-                }
+    Bool using_append = (strcmp(argv[1], "-a") == 0) || (strcmp(argv[1], "--append") == 0);
 
-                int bytes_read = read(fd, buffer, len);
-                if (bytes_read == -1) {
-                    err_exit("read");
-                }
-
-                if (bytes_read == 0) {
-                    printf("%s EOF\n", argv[arg_pair]);
-                } else {
-                    printf("%s: ", argv[arg_pair]);
-                    for (int i = 0; i < bytes_read; i++) {
-                        if (argv[arg_pair][0] == 'r') {
-                            printf("%c", isprint((unsigned char) buffer[i]) ? buffer[i] : '?');
-                        } else {
-                            printf("%02x ", (unsigned int) buffer[i]);
-                        }
-                    }
-                    printf("\n");
-                }
-
-                free(buffer);
-                break;
-            case 'w':
-                bytes_written = write(fd, &argv[arg_pair][1], strlen(&argv[arg_pair][1]));
-                if (bytes_written == -1) {
-                    err_exit("write");
-                }
-
-                printf("%s: wrote %ld bytes\n", argv[arg_pair], (long) bytes_written);
-                break;
-            case 's':
-                offset = get_long(&argv[arg_pair][1], GN_ANY_BASE, argv[arg_pair]);
-                if (lseek(fd, offset, SEEK_SET) == -1) {
-                    err_exit("lseek");
-                }
-
-                printf("%s: seek succeeded\n", argv[arg_pair]);
-                break;
-
-            default:
-                cmd_line_err("Argument must start with [rRws]: %s\n", argv[arg_pair]);
+    if (argc == 2) {
+        if (strcmp(argv[1], "--help") == 0) {
+            usage_err("%s [-a/--append] file\n", argv[0]);
         }
+    } else if (argc == 3) {
+        if (!using_append) {
+            usage_err("%s [-a/--append] file\n", argv[0]);
+        }
+    }
+
+    const char* output_file = using_append ? argv[2] : argv[1];
+
+    int open_flags = O_CREAT | O_RDWR;
+    if (using_append) {
+        open_flags |= O_APPEND;
+    }
+
+    int perm_flags = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+    
+    int fd = open(output_file, open_flags, perm_flags);
+    if (fd == -1) {
+        err_exit("open");
+    }
+
+    char buffer[1];
+    int bytes_read;
+    while((bytes_read = read(0, buffer, sizeof(buffer)) > 0)) {
+        printf("%s", buffer);
+
+        if(write(fd, buffer, bytes_read) != bytes_read) {
+            err_exit("couldn't write whole buffer");
+        }
+    }
+
+    if(close(fd) == -1) {
+        err_exit("close");
     }
     
     return 0;
